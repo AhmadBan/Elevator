0a9536c127e399c790e4ce8d1b213cf0ce868e8d
AhmadBan <Ahmad.Baneshi@gmail.com>
2019-12-15 00:43:23 +0400

    first commit add keil5 project

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,30 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Elevator' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+/*  ARM::CMSIS:RTOS:Keil RTX:4.81.1 */
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+/*  Keil::Device:StdPeriph Drivers:Framework:3.5.1 */
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+/*  Keil::Device:StdPeriph Drivers:GPIO:3.5.0 */
+#define RTE_DEVICE_STDPERIPH_GPIO
+/*  Keil::Device:StdPeriph Drivers:RCC:3.5.0 */
+#define RTE_DEVICE_STDPERIPH_RCC
+
+
+#endif /* RTE_COMPONENTS_H */
